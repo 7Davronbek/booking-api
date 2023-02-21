232feb54731a114b6cc88d2ae912925cb93a0955
7Davronbek <maxmudovdavronbek8@gmail.com>
2023-02-21 21:58:23 +0500

    booking-api

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import authRoute from "./routes/auth.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+import usersRoute from "./routes/users.js";
+
+dotenv.config();
+const app = express();
+mongoose.set("strictQuery", false);
+const connect = async () => {
+  try {
+     mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+// middlewares
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+app.listen(5000, () => {
+  connect();
+  console.log("Backend is running.");
+});
