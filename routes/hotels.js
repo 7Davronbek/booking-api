ba281c5c54c48a8e381a993ecaad6b03d25f56d3
7Davronbek <maxmudovdavronbek8@gmail.com>
2023-02-21 22:39:24 +0500

    booking-api

--- routes/hotels.js
+++ routes/hotels.js
@@ -14,4 +14,48 @@ router.post("/", async (req, res) => {
   }
 });
 
+// PUT
+router.put("/:id", async (req, res) => {
+  try {
+    const updateHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updateHotel);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// DELETE
+router.delete("/:id", async (req, res) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).json("Hotel has been deleted.");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// GET SINGLE HOTEL
+router.get("/:id", async (req, res) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    res.status(200).json(hotel);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// GET ALL HOTELS
+router.get("/", async (req, res) => {
+  try {
+    const hotels = await Hotel.find(req.params.id);
+    res.status(200).json(hotels);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 export default router;
